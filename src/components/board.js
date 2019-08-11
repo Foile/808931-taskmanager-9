a711cc88fc52d5bb08af85078337c8102140030a
foile <foile@mail.ru>
2019-08-11 15:48:02 +0300

    Учебный проект: время разбивать камни

---
+++ src/components/board.js
@@ -0,0 +1,10 @@
+export const board = () => `<section class="board container">
+<div class="board__filter-list">
+  <a href="#" class="board__filter">SORT BY DEFAULT</a>
+  <a href="#" class="board__filter">SORT BY DATE up</a>
+  <a href="#" class="board__filter">SORT BY DATE down</a>
+</div>
+
+<div class="board__tasks">
+</div>
+</section>`;
---
+++ src/components/search.js
@@ -0,0 +1,7 @@
+export const searchMarkup = (classes) => `<section class="${classes}"><input
+type="text"
+id="search__input"
+class="search__input"
+placeholder="START TYPING — SEARCH BY WORD, #HASHTAG OR DATE"
+/>
+<label class="visually-hidden" for="search__input">Search</label></section>`;
