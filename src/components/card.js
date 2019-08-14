c4eb523a0371b0fadb12cfd2cb0784cabf956e78
foile <foile@mail.ru>
2019-08-15 01:45:59 +0300

    Учебный проект: шаблонизируй это

--- src/components/card.js
+++ src/components/card.js
@@ -1,16 +1,18 @@
-export let cardMarkup = (classes) => `<article class="${classes}">
+export let cardMarkup = ({color, description, dueDate, repeatingDays, tags, isFavorite, isArchive}) => `<article
+class="card card--${color}
+${Object.keys(repeatingDays).some((day) => repeatingDays[day]) ? `card--repeat` : ``}">
 <div class="card__form">
   <div class="card__inner">
    <div class="card__control">
      <button type="button" class="card__btn card__btn--edit">
        edit
      </button>
-     <button type="button" class="card__btn card__btn--archive">
+     <button type="button" class="card__btn card__btn--archive ${!isArchive ? `card__btn--disabled` : ``}">
        archive
      </button>
      <button
        type="button"
-       class="card__btn card__btn--favorites card__btn--disabled">
+       class="card__btn card__btn--favorites ${!isFavorite ? `card__btn--disabled` : ``}">
        favorites
      </button>
    </div>
@@ -20,36 +22,26 @@ export let cardMarkup = (classes) => `<article class="${classes}">
      </svg>
    </div>
    <div class="card__textarea-wrap">
-     <p class="card__text">Example default task with default color.</p>
+     <p class="card__text">${description}</p>
    </div>
    <div class="card__settings">
      <div class="card__details">
        <div class="card__dates">
          <div class="card__date-deadline">
            <p class="card__input-deadline-wrap">
-             <span class="card__date">23 September</span>
-             <span class="card__time">11:15 PM</span>
+             <span class="card__date">${new Date(dueDate).toDateString()}</span>
+             <span class="card__time">${new Date(dueDate).toLocaleTimeString()}</span>
            </p>
          </div>
        </div>
 
        <div class="card__hashtag">
          <div class="card__hashtag-list">
-           <span class="card__hashtag-inner">
-             <span class="card__hashtag-name">
-               #todo
-             </span>
-           </span>
-           <span class="card__hashtag-inner">
-             <span class="card__hashtag-name">
-               #personal
-             </span>
-           </span>
-           <span class="card__hashtag-inner">
-             <span class="card__hashtag-name">
-               #important
-             </span>
-           </span>
+         ${Array.from(tags).map((tag) => `<span class="card__hashtag-inner">
+         <span class="card__hashtag-name">
+           #${tag}
+         </span>
+       </span>`).join(``)}
          </div>
        </div>
      </div>
--- src/main.js
+++ src/main.js
@@ -5,14 +5,15 @@ import {cardMarkup} from './components/card';
 import {cardEditMarkup} from './components/card-edit';
 import {board} from './components/board';
 import {loadMoreMarkup} from './components/load-more';
+import {getTask, calcFilters} from './data';
 
 const getMarkup = (blockName, data) => {
   switch (blockName) {
     case `menu`: return menuMarkup(data.classes.join(` `));
     case `search`: return searchMarkup(data.classes.join(` `));
-    case `filter`: return filterMarkup(data.classes.join(` `));
-    case `card`: return cardMarkup(data.classes.join(` `));
-    case `card-form`: return cardEditMarkup(data.classes.join(` `));
+    case `filter`: return filterMarkup(data);
+    case `card`: return cardMarkup(data);
+    case `card-form`: return cardEditMarkup(data);
     case `load-more`: return loadMoreMarkup(data.classes.join(` `));
     case `board`: return board();
     default: throw new Error(`getMarkup: markup not found.`);
@@ -23,28 +24,23 @@ const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
+let editTask = getTask();
+editTask.edit = true;
+const cards = [editTask, getTask(), getTask(), getTask(), getTask(), getTask(), getTask()];
 const mainElement = document.querySelector(`.main`);
 const headerElement = document.querySelector(`.main__control`);
+
 render(headerElement, getMarkup(`menu`, {classes: [`control__btn-wrap`]}), `beforeEnd`);
 render(mainElement, getMarkup(`search`, {classes: [`main__search`, `search`, `container`]}), `beforeEnd`);
-render(mainElement, getMarkup(`filter`, {classes: [`main__filter`, `filter`, `container`]}), `beforeEnd`);
-
-const cards = [{color: `black`, edit: true},
-  {color: `blue`},
-  {color: `yellow`},
-  {color: `green`}
-];
+const filters = calcFilters(cards);
+render(mainElement, getMarkup(`filter`, filters), `beforeEnd`);
 
 render(mainElement, getMarkup(`board`, {}), `beforeEnd`);
 const boardElement = document.querySelector(`.board`);
 const boardTasksElement = boardElement.querySelector(`.board__tasks`);
 
-cards.forEach(({color, edit}) => {
-  let classes = [`card`, `card--${color}`];
-  if (edit) {
-    classes.push(`card--edit`);
-  }
-  render(boardTasksElement, getMarkup(edit ? `card-form` : `card`, {classes}), `beforeEnd`);
+cards.forEach((task) => {
+  render(boardTasksElement, getMarkup(task.edit ? `card-form` : `card`, task), `beforeEnd`);
 });
 
 render(boardElement, getMarkup(`load-more`, {classes: [`load-more`]}), `beforeEnd`);
